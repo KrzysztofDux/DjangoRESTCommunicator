5d9e096e7229fb3d2d79027b936a81c98d92efe7
Krzysztof Kulak <krzysztofpiotrdux@gmail.com>
2019-08-15 17:44:16 +0200

    create Message model and tests for it

--- dispatch/models.py
+++ dispatch/models.py
@@ -10,3 +10,19 @@ class Client(models.Model):
     identity = models.CharField(default=''.join(random.choice(string.digits) for _ in range(IDENTITY_LENGTH)),
                                 max_length=IDENTITY_LENGTH, editable=False)
 
+
+class Message(models.Model):
+    author = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='messages_authored')
+    addressee = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='messages_received')
+    content = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+
+    @staticmethod
+    def get_all_for_client(addressee):
+        if isinstance(addressee, Client):
+            msgs = Message.objects.filter(addressee=addressee).order_by('created')
+        else:
+            msgs = Message.objects.filter(addressee=Client.objects.filter(identity=addressee)).order_by('created')
+        for msg in msgs:
+            msg.delete()
+        return msgs
--- dispatch/tests.py
+++ dispatch/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from dispatch.models import Client
+from dispatch.models import Client, Message
+
+
 # Create your tests here.
 
 
@@ -9,3 +11,63 @@ class ClientTestCase(TestCase):
         client = Client()
         self.assertEqual(len(client.identity), Client.IDENTITY_LENGTH)
 
+
+class MessageTestCase(TestCase):
+
+    def test_get_all_for_client_returns_all_messages(self):
+        addressee = Client()
+        addressee.save()
+
+        orig_msgs = MessageTestCase.create_messages_to(addressee)
+
+        ret_msgs = Message.get_all_for_client(addressee)
+
+        self.assertEqual(4, ret_msgs.count())
+
+        for ret, orig in zip(ret_msgs, orig_msgs):
+            self.assertEqual(ret.content, orig.content)
+            self.assertEqual(ret.author, orig.author)
+            self.assertEqual(ret.addressee, orig.addressee)
+
+    def test_get_all_for_client_deletes_messages(self):
+        addressee = Client()
+        addressee.save()
+
+        MessageTestCase.create_messages_to(addressee)
+
+        first_call_results = Message.get_all_for_client(addressee)
+
+        second_call_results = Message.get_all_for_client(addressee)
+
+        self.assertEqual(0, second_call_results.count())
+
+    def test_get_all_for_client_returns_messages_for_addressee_only(self):
+        addressee = Client()
+        addressee.save()
+        other_addressee = Client()
+        other_addressee.save()
+
+        orig_msgs = MessageTestCase.create_messages_to(addressee)
+        other_msgs = MessageTestCase.create_messages_to(other_addressee)
+
+        ret_msgs = Message.get_all_for_client(addressee)
+
+        self.assertEqual(4, ret_msgs.count())
+
+        for ret, orig in zip(ret_msgs, orig_msgs):
+            self.assertEqual(ret.addressee, orig.addressee)
+
+    @staticmethod
+    def create_messages_to(addressee):
+        author_one = Client()
+        author_one.save()
+        author_two = Client()
+        author_two.save()
+
+        msgs = [Message(author=author_one, addressee=addressee, content="abc"),
+                Message(author=author_one, addressee=addressee, content="abcd"),
+                Message(author=author_two, addressee=addressee, content="abcde"),
+                Message(author=author_two, addressee=addressee, content="abcdef")]
+        for msg in msgs:
+            msg.save()
+        return msgs
